19fe528a9bded42dfd38c5947cb076976de8d455
Anh Cao <anhcao4922@gmail.com>
2024-06-10 22:48:18 +0300

    refactor CurrentPriceView into 2 files: existing and PriceView
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- Sthor/Sources/Views/Dashboard/CurrentPriceView.swift
+++ Sthor/Sources/Views/Dashboard/CurrentPriceView.swift
@@ -8,11 +8,9 @@
 import SwiftUI
 
 struct CurrentPriceView: View {
-//    let price: TodayTomorrowPrices = sampleTodayPricesOnly
     @StateObject var viewModel = CurrentPriceViewModel()
     
     var body: some View {
-
         GroupBox("Exchange price of electric now") {
             switch viewModel.currentPriceState {
             case .loading:
@@ -20,48 +18,8 @@ struct CurrentPriceView: View {
             case .failure:
                 Text(viewModel.errorMessage)
             case .success:
-                if let prices = viewModel.currentPrices {
-                    HStack {
-                        VStack {
-                            VStack {
-                                Text(viewModel.getCurrentPrice(todayPrices: prices.today.prices))
-                                    .font(.title)
-                                    .bold()
-                                Text(prices.today.prices.name)
-                            }
-                            .frame(maxWidth: 100, alignment: .center)
-                            
-                        }
-                        Spacer()
-                        VStack{
-                            // TODO: The price of next day often available around 3pm. Shall we consider to polling the backend from 14:30 - 15:30 after every 10 minutes
-                            if prices.today.available {
-                                MiniExchangePricesBarChart(data: prices.today.prices)
-                                    .frame(height: 50)
-                            } else {
-                                // TODO: Consider for later if we decide to support different languages
-                                HStack {
-                                    Text("Today's prices are not available yet")
-                                    Spacer()
-                                }
-                            }
-                            
-                            if prices.tomorrow.available {
-                                MiniExchangePricesBarChart(data: prices.tomorrow.prices)
-                                    .frame(height: 50)
-                            } else {
-                                // TODO: Consider for later if we decide to support different languages
-                                HStack {
-                                    Text("Tomorrow's prices are not available yet")
-                                    Spacer()
-                                }
-                            }
-                        }
-                    }
-                    .font(.caption)
-                    .frame(maxHeight: 150)
-                }
-            }            
+                PriceView(viewModel: viewModel)
+            }
         }
     }
 }
---
+++ Sthor/Sources/Views/Dashboard/PriceView.swift
@@ -0,0 +1,60 @@
+//
+//  PriceView.swift
+//  Sthor
+//
+//  Created by Anh Cao on 10.6.2024.
+//
+
+import SwiftUI
+
+struct PriceView: View {
+    @ObservedObject public var viewModel: CurrentPriceViewModel
+    
+    var body: some View {
+        if let prices = viewModel.currentPrices {
+            HStack {
+                VStack {
+                    VStack {
+                        Text(viewModel.getCurrentPrice(todayPrices: prices.today.prices))
+                            .font(.title)
+                            .bold()
+                        Text(prices.today.prices.name)
+                    }
+                    .frame(maxWidth: 100, alignment: .center)
+                    
+                }
+                Spacer()
+                VStack{
+                    // TODO: The price of next day often available around 3pm. Shall we consider to polling the backend from 14:30 - 15:30 after every 10 minutes
+                    if prices.today.available {
+                        MiniExchangePricesBarChart(data: prices.today.prices)
+                            .frame(height: 50)
+                    } else {
+                        // TODO: Consider for later if we decide to support different languages
+                        HStack {
+                            Text("Today's prices are not available yet")
+                            Spacer()
+                        }
+                    }
+                    
+                    if prices.tomorrow.available {
+                        MiniExchangePricesBarChart(data: prices.tomorrow.prices)
+                            .frame(height: 50)
+                    } else {
+                        // TODO: Consider for later if we decide to support different languages
+                        HStack {
+                            Text("Tomorrow's prices are not available yet")
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            .font(.caption)
+            .frame(maxHeight: 150)
+        }
+    }
+}
+
+#Preview {
+    PriceView(viewModel: CurrentPriceViewModel())
+}

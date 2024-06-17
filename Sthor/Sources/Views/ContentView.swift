0e7b9e97f30f2f3bcb2b6263bf2cc0fd7b8ceb5a
Anh Cao <anhcao4922@gmail.com>
2024-06-17 23:55:37 +0300

    implement the binding function to listen on tab view
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- Sthor/Sources/Views/ContentView.swift
+++ Sthor/Sources/Views/ContentView.swift
@@ -10,8 +10,29 @@ import SwiftUI
 struct ContentView: View {
     @State private var selectedTab: Tab = .home
     
+    var tabHandler: Binding<Tab> { Binding(
+            get: { self.selectedTab },
+            set: { selectedTab in
+                switch selectedTab {
+                case .home:
+                    print("Reset to home!!")
+//                    withAnimation{
+//                        
+//                    }
+                case .profile:
+                    print("Reset to profile!!")
+//                    withAnimation {
+//                        
+//                    }
+                }
+                
+                // Make sure the new value is persisted.
+                self.selectedTab = selectedTab
+            }
+        )}
+    
     var body: some View {
-        TabView(selection: $selectedTab) {
+        TabView(selection: tabHandler) {
             HomeView()
                 .tabItem{
                     Label("Home", systemImage: "house")

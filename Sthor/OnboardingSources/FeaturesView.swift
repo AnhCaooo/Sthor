26f9bcc67659432586abd7db376fe8af79b8e39f
Anh Cao <anhcao4922@gmail.com>
2024-06-30 21:15:51 +0300

    add feature items to features page
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- Sthor/OnboardingSources/FeaturesView.swift
+++ Sthor/OnboardingSources/FeaturesView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct FeaturesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+            
+            FeatureCard(iconName: "chart.bar.xaxis.ascending.badge.clock",
+                        description: "Get visualization view about exchange electric price in Finland")
+            FeatureCard(iconName: "bell.and.waves.left.and.right.fill",
+                        description: "Get notifications when tomorrow's exchange prices are available")
+            FeatureCard(iconName: "apps.iphone",
+                        description: "View price status via Widget without opening the app")
+        }
+        .padding()
     }
 }
 
 #Preview {
     FeaturesView()
 }
+

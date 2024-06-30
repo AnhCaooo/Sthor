0b780469d617a2288e678f497cff710dba629878
Anh Cao <anhcao4922@gmail.com>
2024-06-30 21:15:11 +0300

    init views for onboarding page
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

---
+++ Sthor/OnboardingSources/Components/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  Sthor
+//
+//  Created by Anh Cao on 30.6.2024.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
---
+++ Sthor/OnboardingSources/FeaturesView.swift
@@ -0,0 +1,18 @@
+//
+//  FeaturesView.swift
+//  Sthor
+//
+//  Created by Anh Cao on 30.6.2024.
+//
+
+import SwiftUI
+
+struct FeaturesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeaturesView()
+}

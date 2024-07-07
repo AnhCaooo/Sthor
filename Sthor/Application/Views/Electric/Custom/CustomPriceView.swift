63834a62c977165af76abb1b225131421975cfd2
Anh Cao <anhcao4922@gmail.com>
2024-07-07 11:36:29 +0300

    create customPrice view and arrange weekly, monthly view
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

---
+++ Sthor/Application/Views/Electric/Custom/CustomPriceView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPriceView.swift
+//  Sthor
+//
+//  Created by Anh Cao on 7.7.2024.
+//
+
+import SwiftUI
+
+struct CustomPriceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomPriceView()
+}

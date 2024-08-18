a7b8c2c106773b5f3047798faaa6db1cd51a5dea
Anh Cao <anhcao4922@gmail.com>
2024-08-18 19:55:33 +0300

    create common swiftui files for all types of electric view
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

---
+++ Sthor/Application/Views/Electric/ElectricDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ElectricDetailsView.swift
+//  Sthor
+//
+//  Created by Anh Cao on 18.8.2024.
+//
+
+import SwiftUI
+
+struct ElectricDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ElectricDetailsView()
+}
